c463569a5f26c801b9f1c003f2c7853852b4e621
Ivan <vkorootaev1@mail.ru>
2022-12-05 22:26:57 +0500

    05.12.22 21:00

--- mysite/quiz/admin.py
+++ mysite/quiz/admin.py
@@ -1,10 +1,39 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
 from .models import *
 
-admin.site.register(User)
-admin.site.register(House)
-admin.site.register(Quiz)
-admin.site.register(Answer)
-admin.site.register(News)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'house', 'flat')
+    list_display_links = ('username', )
+    search_fields = ('username', 'email')
+
+
+class HouseAdmin(admin.ModelAdmin):
+    list_display = ('street', 'house', 'house_index', 'max_flat')
+    list_display_links = ('street', 'house')
+    search_fields = ('street', 'house', 'house_index')
+
+
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'finished_at')
+    list_display_links = ('title', )
+    search_fields = ('title', )
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('quiz', 'answer', 'user', 'created_at')
+    list_display_links = ('quiz', 'user')
+    search_fields = ('quiz', 'user')
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    list_display_links = ('title', )
+    search_fields = ('title', )
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(House, HouseAdmin)
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Answer, AnswerAdmin)
+admin.site.register(News, NewsAdmin)
--- mysite/quiz/models.py
+++ mysite/quiz/models.py
@@ -4,20 +4,23 @@ from django.urls import reverse
 
 
 class User(AbstractUser):
-    house = models.ForeignKey('House', on_delete=models.CASCADE)
-    flat = models.IntegerField()
-    email = models.EmailField(unique=True)
+    house = models.ForeignKey('House', on_delete=models.CASCADE, verbose_name='Дом')
+    flat = models.IntegerField(verbose_name='Квартира')
+    email = models.EmailField(unique=True, verbose_name='Электронная почта')
     email_verify = models.BooleanField(default=False)
 
     class Meta:
         unique_together = ['house', 'flat']
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ['id', 'username']
 
 
 class House(models.Model):
-    street = models.CharField(max_length=150)
-    house = models.CharField(max_length=150)
-    house_index = models.IntegerField()
-    max_flat = models.IntegerField()
+    street = models.CharField(max_length=150, verbose_name='Улица')
+    house = models.CharField(max_length=150, verbose_name='Номер дома')
+    house_index = models.IntegerField(verbose_name='Индекс дома')
+    max_flat = models.IntegerField(verbose_name='Количество квартир в доме')
 
     def __str__(self):
         return f"{self.street} {self.house}"
@@ -27,15 +30,18 @@ class House(models.Model):
 
     class Meta:
         unique_together = ['street', 'house']
+        verbose_name = "Дом"
+        verbose_name_plural = "Дома"
+        ordering = ['street', 'house']
 
 
 class Quiz(models.Model):
-    title = models.CharField(max_length=150)
-    text = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    house = models.ManyToManyField(House)
-    finished_at = models.DateTimeField()
+    title = models.CharField(max_length=150, verbose_name='Заголовок')
+    text = models.TextField(blank=True, verbose_name='Текст опроса')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    house = models.ManyToManyField(House, 'Дом')
+    finished_at = models.DateTimeField(verbose_name='Дата окончания')
 
     def __str__(self):
         return f"{self.title}"
@@ -47,7 +53,9 @@ class Quiz(models.Model):
         return reverse('manage_answers', kwargs={'house_id': self.house, 'quiz_id': self.pk})
 
     class Meta:
-        pass
+        verbose_name = "Опрос"
+        verbose_name_plural = "Опросы"
+        ordering = ['created_at', '-finished_at']
 
 
 answer_choice = [
@@ -57,11 +65,11 @@ answer_choice = [
 
 
 class Answer(models.Model):
-    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    answer = models.CharField(choices=answer_choice, max_length=10)
-    created_at = models.DateTimeField(auto_now=True)
-    comment = models.TextField(blank=True)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, verbose_name='Опрос')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    answer = models.CharField(choices=answer_choice, max_length=10, verbose_name='Ответ')
+    created_at = models.DateTimeField(auto_now=True, verbose_name='Дата создания')
+    comment = models.TextField(blank=True, verbose_name='Комментарий')
 
     def get_display_answer(self):
         return self.get_answer_display()
@@ -74,11 +82,18 @@ class Answer(models.Model):
 
     class Meta:
         unique_together = ['quiz', 'user']
+        verbose_name = "Ответ"
+        verbose_name_plural = "Ответы"
+        ordering = ['id', 'created_at']
 
 
 class News(models.Model):
-    title = models.CharField(max_length=100)
-    text = models.TextField()
-    created_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст новости')
+    created_at = models.DateTimeField(auto_now=True, verbose_name='Дата создания новости')
 
+    class Meta:
+        verbose_name = "Новость"
+        verbose_name_plural = "Новости"
+        ordering = ['id', 'created_at']
 
